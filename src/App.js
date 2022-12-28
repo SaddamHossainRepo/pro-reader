cb399ce7e5a0451b507bf0ffc22269fb072530db
Saddam <saddam13@cse.pstu.ac.bd>
2022-12-29 01:35:22 +0600

    added book details

--- src/App.js
+++ src/App.js
@@ -1,42 +1,8 @@
 import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Header from './components/Header'
-import Home from './components/Home';
-import Books from './components/Books'
-import ErrorPage from './components/ErrorPage';
-import About from './components/About';
-import Root from './components/Root';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root></Root>,
-    errorElement: <ErrorPage></ErrorPage>,
-    children: [
-      {
-        path: '/',
-        element: <Home></Home>
-      },
-      {
-        path: 'home',
-        element: <Home></Home>,
-        
-      },
-      {
-        path: '/books',
-        element: <Books></Books>,
-      },
-      {
-        path: '/about',
-        element: <About></About>,
-      },
-    ]
-  },
-  
- 
+import { RouterProvider } from 'react-router-dom'
+import router from './Routes';
 
 
-])
 
 function App() {
   return (
---
+++ src/Routes.js
@@ -0,0 +1,45 @@
+import { createBrowserRouter } from 'react-router-dom';
+import About from './components/About';
+import Books from './components/Books';
+import ErrorPage from './components/ErrorPage';
+import Home from './components/Home';
+import Root from './components/Root';
+import BookDetails from './components/BookDetails';
+
+const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Root></Root>,
+      errorElement: <ErrorPage></ErrorPage>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: 'home',
+          element: <Home></Home>,
+          
+        },
+        {
+          path: '/books',
+          loader: ()=> {
+            return fetch('https://api.itbook.store/1.0/new')
+          },
+          element: <Books></Books>,
+        },
+        {
+          path: '/about',
+          element: <About></About>,
+        },
+        {
+          path: '/book/:id',
+          element: <BookDetails></BookDetails>,
+          loader: ({params}) => fetch(`https://api.itbook.store/1.0/books/${params.id}`),
+        },
+        
+      ]
+    },
+  ])
+
+export default router
